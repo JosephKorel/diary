d4b844a9466004e3aa04c619d8d3e3ff311751f5
José Carlos Rolim Etchepare <jcjosecarlos03@gmail.com>
2022-10-06 20:07:00 -0300

    google client id

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,8 +4,8 @@ import GoogleProvider from "next-auth/providers/google";
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
   secret: "9511af3fb76533fa77bc6ba1f7d8e162",
